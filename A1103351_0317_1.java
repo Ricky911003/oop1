690095dfc5b6c7caaf116c7e0779fb44ddb4d14f
Ricky911003 <121994840+Ricky911003@users.noreply.github.com>
2023-03-21 14:39:53 +0800

    Add files via upload

--- A1103351_0317_1.java
+++ A1103351_0317_1.java
@@ -5,13 +5,14 @@ public class A1103351_0317_1 {
         Scanner input = new Scanner(System.in);
         System.out.printf("請輸入n值: ");
         int n = input.nextInt();
-        System.out.printf("請輸入n值: ");
+        System.out.printf("請輸入m值: ");
         int m = input.nextInt();
 
         int nm[][] = new int[n][m];
         for(int i = 0; i <nm.length; i++){
             for(int j = 0; j<nm[i].length ; j++){
                 nm[i][j]=(i+1)*(j+1);
+                
                 // System.out.printf("%d", nm[i][j]);
             }
             // System.out.printf("\n");
